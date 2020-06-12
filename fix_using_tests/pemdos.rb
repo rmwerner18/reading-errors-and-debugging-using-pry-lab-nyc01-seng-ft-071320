84910da797b0ab3bb1d3b9b21c9ec7415c76ffda
Ryan Werner <rmwerner@umich.edu>
2020-06-12 23:01:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    ss = (10.times do print "s")
+    ss = 10.times do 
+      print "s"
     end
     binding.pry
       "#{ss}" + "#{string}"
